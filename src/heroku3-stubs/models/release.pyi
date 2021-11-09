b03adaf4d0197c7bab1745d0deba2f4ae50826ba
Andrew Marshall <andrew.marshall@vts.com>
2021-11-09 23:01:18 +0000

    heroku3: Improve typing for Release

--- src/heroku3-stubs/models/release.pyi
+++ src/heroku3-stubs/models/release.pyi
@@ -1,8 +1,14 @@
 from . import BaseResource as BaseResource, User as User
 from .slug import Slug as Slug
+import datetime as dt
 from typing import Any
 
 class Release(BaseResource):
     order_by: str
     app: Any
+    description: str
+    updated_at: dt.datetime
+    id: str
+    status: str
+
     def __init__(self) -> None: ...
